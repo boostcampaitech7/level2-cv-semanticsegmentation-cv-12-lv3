2f53b20c8746bbbff89775aebd13c62f007d9a5b
lkl4502 <lkl4502@naver.com>
2024-11-11 15:59:02 +0000

    [#5] ✨ Implement base_model module in project structure

---
+++ models/base_model.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+
+from torchvision import models
+
+class TorchvisionModel(nn.Module):
+    """
+    Baseline Model from torchvision
+    """
+    def __init__(self,
+                 model_name: str,
+                 num_classes: int,
+                 pretrained: bool):
+        super(TorchvisionModel, self).__init__()
+        self.model = models.__dict__[model_name](pretrained=pretrained)
+
+        old_head = self.model.classifier
+        last_conv = list(old_head.children())[-1]
+
+        self.model.classifier = nn.Sequential(
+            *list(old_head.children())[:-1],
+            nn.Conv2d(
+                in_channels=last_conv.in_channels,
+                out_channels=num_classes,
+                kernel_size=last_conv.kernel_size,
+                stride=last_conv.stride,
+                padding=last_conv.stride,
+            ),
+        )
+
+    def forward(self, x: torch.Tensor):
+        return self.model(x)

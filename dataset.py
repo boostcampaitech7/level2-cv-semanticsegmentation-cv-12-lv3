ac544688c6668dfe5a941178026e4b6b90cfe209
lkl4502 <lkl4502@naver.com>
2024-11-11 15:33:33 +0000

    [#5] ✨ Implement dataset module in project structure

---
+++ dataset.py
@@ -0,0 +1,101 @@
+import os
+import cv2
+import json
+import torch
+import numpy as np
+import os.path as osp
+
+from torch.utils.data import Dataset
+from sklearn.model_selection import GroupKFold
+
+CLASSES = [
+    'finger-1', 'finger-2', 'finger-3', 'finger-4', 'finger-5',
+    'finger-6', 'finger-7', 'finger-8', 'finger-9', 'finger-10',
+    'finger-11', 'finger-12', 'finger-13', 'finger-14', 'finger-15',
+    'finger-16', 'finger-17', 'finger-18', 'finger-19', 'Trapezium',
+    'Trapezoid', 'Capitate', 'Hamate', 'Scaphoid', 'Lunate',
+    'Triquetrum', 'Pisiform', 'Radius', 'Ulna',
+]
+
+CLASS2IND = {v: i for i, v in enumerate(CLASSES)}
+IND2CLASS = {v: k for k, v in CLASS2IND.items()}
+
+class XRayDataset(Dataset):
+    def __init__(self, fnames, labels, image_root, label_root, transforms=None, is_train=True):
+        self.transforms = transforms
+        self.is_train = is_train
+        self.image_root = image_root
+        self.label_root = label_root
+        
+        groups = [osp.dirname(fname) for fname in self.fnames]
+        
+        # dummy label
+        ys = [0] * len(self.fnames)
+        
+        gkf = GroupKFold(n_splits=5)
+        
+        filenames = []
+        labelnames = []
+        for i, (x, y) in enumerate(gkf.split(fnames, ys, groups)):
+            if self.is_train:
+                if i == 0:
+                    continue        
+                filenames += list(fnames[y])
+                labelnames += list(labels[y])
+            
+            else:
+                filenames = list(fnames[y])
+                labelnames = list(labels[y])
+                break
+        
+        self.fnames = filenames
+        self.labels = labelnames
+    
+    def __len__(self):
+        return len(self.fnames)
+    
+    def __getitem__(self, item):
+        image_name = self.fnames[item]
+        image_path = os.path.join(self.image_root, image_name)
+        
+        image = cv2.imread(image_path)
+        image = image / 255.
+        
+        label_name = self.labels[item]
+        label_path = os.path.join(self.label_root, label_name)
+        
+        # (H, W, NC) 모양의 label을 생성합니다.
+        label_shape = tuple(image.shape[:2]) + (len(CLASSES), )
+        label = np.zeros(label_shape, dtype=np.uint8)
+        
+        # label 파일을 읽습니다.
+        with open(label_path, "r") as f:
+            annotations = json.load(f)
+        annotations = annotations["annotations"]
+        
+        # 클래스 별로 처리합니다.
+        for ann in annotations:
+            c = ann["label"]
+            class_ind = CLASS2IND[c]
+            points = np.array(ann["points"])
+            
+            # polygon 포맷을 dense한 mask 포맷으로 바꿉니다.
+            class_label = np.zeros(image.shape[:2], dtype=np.uint8)
+            cv2.fillPoly(class_label, [points], 1)
+            label[..., class_ind] = class_label
+        
+        if self.transforms is not None:
+            inputs = {"image": image, "mask": label} if self.is_train else {"image": image}
+            result = self.transforms(**inputs)
+            
+            image = result["image"]
+            label = result["mask"] if self.is_train else label
+
+        # to tenser will be done later
+        image = image.transpose(2, 0, 1)    # channel first 포맷으로 변경합니다.
+        label = label.transpose(2, 0, 1)
+        
+        image = torch.from_numpy(image).float()
+        label = torch.from_numpy(label).float()
+            
+        return image, label

9ecbd40ae9e7d4ad80130a461bd015e668acaeef
lkl4502 <lkl4502@naver.com>
2024-11-12 12:43:49 +0000

    [#5] ✨ Implement Model Selector

--- models/base_model.py
+++ models/base_model.py
@@ -7,15 +7,9 @@ class UnetModel(nn.Module):
     Base Model Unet
     """
     def __init__(self,
-                 encoer_name: str,
-                 encoder_weights="imagenet",
-                 in_channels=3,
-                 classes=29):
+                 **kwargs):
         super(UnetModel, self).__init__()
-        self.model = smp.Unet(encoer_name=encoer_name,
-                              encoder_weights=encoder_weights,
-                              in_channels=in_channels,
-                              classes=classes)
+        self.model = smp.Unet(**kwargs)
 
     def forward(self, x: torch.Tensor):
         return self.model(x)

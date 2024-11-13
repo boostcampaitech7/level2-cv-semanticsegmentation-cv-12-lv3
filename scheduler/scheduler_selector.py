d93a37551c05b317dc20099a8c83bae60e26d752
lkl4502 <lkl4502@naver.com>
2024-11-13 06:10:42 +0000

    [#30] ✨ Add ReduceLROnPlateau

--- scheduler/scheduler_selector.py
+++ scheduler/scheduler_selector.py
@@ -4,6 +4,10 @@ from torch.optim import lr_scheduler
 def multi_step_lr(optimizer, **scheduler_parameter):
     return lr_scheduler.MultiStepLR(optimizer, **scheduler_parameter)
 
+# ReduceLROnPlateau
+def reduce_lr_on_plateau(optimizer, **scheduler_parameter):
+    return lr_scheduler.ReduceLROnPlateau(optimizer, **scheduler_parameter)
+
 # CosineAnnealingLR
 def cosine_annealing_lr(optimizer, **scheduler_parameter):
     return lr_scheduler.CosineAnnealingLR(optimizer, **scheduler_parameter)
@@ -20,6 +24,7 @@ class SchedulerSelector():
     def __init__(self, optimizer) -> None:
         self.scheduler_classes = {
             "MultiStepLR" : multi_step_lr,
+            "ReduceLROnPlateau" : reduce_lr_on_plateau,
             "CosineAnnealingLR" : cosine_annealing_lr
         }
         self.optimizer = optimizer

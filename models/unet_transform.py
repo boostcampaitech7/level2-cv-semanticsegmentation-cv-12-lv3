926aecc68196eaa588556e4f41063d19380d2cce
yejin-s9 <yeajin9940@gmail.com>
2024-11-13 06:21:26 +0000

    ♻️[# 23] Update unet_transform

--- models/unet_transform.py
+++ models/unet_transform.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import init
+import segmentation_models_pytorch as smp
+
 
 def init_weights(net, init_type='normal', gain=0.02):
     def init_func(m):
@@ -357,79 +359,79 @@ class AttU_Net(nn.Module):
 
 
 class R2AttU_Net(nn.Module):
-    def __init__(self,img_ch=3,output_ch=1,t=2):
-        super(R2AttU_Net,self).__init__()
-        
-        self.Maxpool = nn.MaxPool2d(kernel_size=2,stride=2)
-        self.Upsample = nn.Upsample(scale_factor=2)
+    def __init__(self, encoder_name='vgg16', encoder_weights='imagenet', output_ch=29, t=2):
+        super(R2AttU_Net, self).__init__()
 
-        self.RRCNN1 = RRCNN_block(ch_in=img_ch,ch_out=64,t=t)
+        # SMP 라이브러리를 사용해 VGG16 백본을 가져오기
+        self.encoder = smp.Unet(
+            encoder_name=encoder_name,       # VGG16 백본 사용
+            encoder_weights=encoder_weights, # ImageNet 사전 학습된 가중치 사용
+            in_channels=3,
+            classes=output_ch
+        )
 
-        self.RRCNN2 = RRCNN_block(ch_in=64,ch_out=128,t=t)
-        
-        self.RRCNN3 = RRCNN_block(ch_in=128,ch_out=256,t=t)
-        
-        self.RRCNN4 = RRCNN_block(ch_in=256,ch_out=512,t=t)
-        
-        self.RRCNN5 = RRCNN_block(ch_in=512,ch_out=1024,t=t)
-        
+        # RRCNN 블록과 Attention 블록 추가
+        self.RRCNN1 = RRCNN_block(ch_in=64, ch_out=64, t=t)
+        self.RRCNN2 = RRCNN_block(ch_in=128, ch_out=128, t=t)
+        self.RRCNN3 = RRCNN_block(ch_in=256, ch_out=256, t=t)
+        self.RRCNN4 = RRCNN_block(ch_in=512, ch_out=512, t=t)
+        self.RRCNN5 = RRCNN_block(ch_in=512, ch_out=1024, t=t)
 
-        self.Up5 = up_conv(ch_in=1024,ch_out=512)
-        self.Att5 = Attention_block(F_g=512,F_l=512,F_int=256)
-        self.Up_RRCNN5 = RRCNN_block(ch_in=1024, ch_out=512,t=t)
-        
-        self.Up4 = up_conv(ch_in=512,ch_out=256)
-        self.Att4 = Attention_block(F_g=256,F_l=256,F_int=128)
-        self.Up_RRCNN4 = RRCNN_block(ch_in=512, ch_out=256,t=t)
-        
-        self.Up3 = up_conv(ch_in=256,ch_out=128)
-        self.Att3 = Attention_block(F_g=128,F_l=128,F_int=64)
-        self.Up_RRCNN3 = RRCNN_block(ch_in=256, ch_out=128,t=t)
-        
-        self.Up2 = up_conv(ch_in=128,ch_out=64)
-        self.Att2 = Attention_block(F_g=64,F_l=64,F_int=32)
-        self.Up_RRCNN2 = RRCNN_block(ch_in=128, ch_out=64,t=t)
+        self.Up5 = up_conv(ch_in=1024, ch_out=512)
+        self.Att5 = Attention_block(F_g=512, F_l=512, F_int=256)
+        self.Up_RRCNN5 = RRCNN_block(ch_in=1024, ch_out=512, t=t)
 
-        self.Conv_1x1 = nn.Conv2d(64,output_ch,kernel_size=1,stride=1,padding=0)
+        self.Up4 = up_conv(ch_in=512, ch_out=256)
+        self.Att4 = Attention_block(F_g=256, F_l=256, F_int=128)
+        self.Up_RRCNN4 = RRCNN_block(ch_in=512, ch_out=256, t=t)
 
+        self.Up3 = up_conv(ch_in=256, ch_out=128)
+        self.Att3 = Attention_block(F_g=128, F_l=128, F_int=64)
+        self.Up_RRCNN3 = RRCNN_block(ch_in=256, ch_out=128, t=t)
 
-    def forward(self,x):
-        # encoding path
-        x1 = self.RRCNN1(x)
+        self.Up2 = up_conv(ch_in=128, ch_out=64)
+        self.Att2 = Attention_block(F_g=64, F_l=64, F_int=32)
+        self.Up_RRCNN2 = RRCNN_block(ch_in=128, ch_out=64, t=t)
 
-        x2 = self.Maxpool(x1)
+        self.Conv_1x1 = nn.Conv2d(64, output_ch, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x):
+        # SMP 인코더 사용
+        features = self.encoder.encoder(x)
+
+        x1 = features[0]  # Stage 1
+        x2 = features[1]  # Stage 2
+        x3 = features[2]  # Stage 3
+        x4 = features[3]  # Stage 4
+        x5 = features[4]  # Stage 5
+
+        # RRCNN 블록
+        x1 = self.RRCNN1(x1)
         x2 = self.RRCNN2(x2)
-        
-        x3 = self.Maxpool(x2)
         x3 = self.RRCNN3(x3)
-
-        x4 = self.Maxpool(x3)
         x4 = self.RRCNN4(x4)
-
-        x5 = self.Maxpool(x4)
         x5 = self.RRCNN5(x5)
 
-        # decoding + concat path
+        # 디코딩 및 Attention 단계
         d5 = self.Up5(x5)
-        x4 = self.Att5(g=d5,x=x4)
-        d5 = torch.cat((x4,d5),dim=1)
+        x4 = self.Att5(g=d5, x=x4)
+        d5 = torch.cat((x4, d5), dim=1)
         d5 = self.Up_RRCNN5(d5)
-        
+
         d4 = self.Up4(d5)
-        x3 = self.Att4(g=d4,x=x3)
-        d4 = torch.cat((x3,d4),dim=1)
+        x3 = self.Att4(g=d4, x=x3)
+        d4 = torch.cat((x3, d4), dim=1)
         d4 = self.Up_RRCNN4(d4)
 
         d3 = self.Up3(d4)
-        x2 = self.Att3(g=d3,x=x2)
-        d3 = torch.cat((x2,d3),dim=1)
+        x2 = self.Att3(g=d3, x=x2)
+        d3 = torch.cat((x2, d3), dim=1)
         d3 = self.Up_RRCNN3(d3)
 
         d2 = self.Up2(d3)
-        x1 = self.Att2(g=d2,x=x1)
-        d2 = torch.cat((x1,d2),dim=1)
+        x1 = self.Att2(g=d2, x=x1)
+        d2 = torch.cat((x1, d2), dim=1)
         d2 = self.Up_RRCNN2(d2)
 
         d1 = self.Conv_1x1(d2)
-
         return d1

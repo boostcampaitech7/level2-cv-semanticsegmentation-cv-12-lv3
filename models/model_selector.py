fa4cb97ccf6edf7e0ba981a61304732720f30586
john8538@naver.com <john8538@naver.com>
2024-11-21 07:32:45 +0000

    [#81] ✨ Add Upernet

--- models/model_selector.py
+++ models/model_selector.py
@@ -4,6 +4,8 @@ from .unet_transform import R2U_Net, AttU_Net, R2AttU_Net
 from .effisegnet import EffiSegNetBN
 from .upernet import UperNet
 from .mask2former import Mask2Former
+from .segformer import SegFormerModel
+
 class ModelSelector():
     """
     model을 새롭게 추가하기 위한 방법
@@ -25,7 +27,8 @@ class ModelSelector():
             "Swin" : SwinUnet,
             "EffiSegNet": EffiSegNetBN,
             "UperNet": UperNet,
-            "Mask2Former": Mask2Former
+            "Mask2Former": Mask2Former,
+            "SegFormer": SegFormerModel
         }
 
     def get_model(self, model_name, **model_parameter):

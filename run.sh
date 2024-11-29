daa3f9cd45c45ade656fc09200e4bb2d1f8d81aa
오홍석 <lkl4502@kookmin.ac.kr>
2024-11-29 16:23:30 +0900

    [#91] 🐛 run.sh 삭제

--- run.sh
+++
@@ -1,7 +0,0 @@
-#! bin/bash
-
-python train.py --config /data/ephemeral/home/lexxsh/level2-cv-semanticsegmentation-cv-12-lv3/configs/eff_v2.yaml
-python inference.py ./checkpoints/Unet/eff_v2/best* --output output/eff_v2_output.csv --resize 2048
-
-python train.py --config /data/ephemeral/home/lexxsh/level2-cv-semanticsegmentation-cv-12-lv3/configs/vgg.yaml
-python inference.py ./checkpoints/Unet/vgg/best* --output output/vgg_output.csv --resize 2048
